8871d2858b9aadc0c095c8932a54ff570791f528
JD Tadlock <jt7903@gmail.com>
2023-06-01 12:11:30 -0400

    starting functional programming

--- js/script.js
+++ js/script.js
@@ -36,25 +36,15 @@ var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '<', '>', '.', '?', '/'];
-var combined = lowercase.concat(uppercase).concat(numbers).concat(specialCharacters);
+var combined = [];
 
-// Create an amount of characters that I want to generate
-var amount = 50;
 
-// Create a variable that will hold the resulting finished string
-var password = '';
 
-// Loop the given amount and generate a random character from the array
-for (var count = 0; count < amount; count++) {
-  var randomNum = Math.random();
-  var randomIndex = Math.floor(randomNum * combined.length);
 
-  // Concatenate the random letter onto the end of the result string
-  password += combined[randomIndex];
-}
 
-// After the loop completes, print the result to the console
-console.log(password);
+
+
+
 
 
 
