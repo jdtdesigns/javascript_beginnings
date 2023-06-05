4e67e5cd1d48cd05a831154af819f85cdaf980c8
JD Tadlock <jt7903@gmail.com>
2023-06-05 13:48:47 -0400

    object basics complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,127 @@
+// var fruits = ['apple', 'grape', 'kiwi'];
+
+
+var users = [
+  {
+    name: 'JD',
+    age: 43
+  },
+  {
+    name: 'Jane',
+    age: 35,
+    hobbies: ['tennis', 'hiking']
+  },
+  {
+    name: 'Bob',
+    healthcare: {
+      locations: {
+        pharmacy: '555 Cool St.',
+        emergency: '555 Help Dr.'
+      },
+      medications: {
+        amount: 50,
+        types: [
+          {
+            bloodPressure: 'Amlodapine',
+            pillSize: '10mg'
+          },
+          {
+            painRelief: 'Tylenol',
+            pillSize: '500mg'
+          }
+        ]
+      }
+    },
+    age: 99
+  }
+];
+
+// var jane = users.find(function (userObj) {
+//   if (userObj.name === 'Jane') return true;
+// });
+
+// console.log(jane);
+
+// console.log(users[2].healthcare.medications.types[1].pillSize);
+
+
+// var str = 'some string';
+
+// str = str.slice(3);
+
+// console.log(str);
+// console.log(str.length);
+
+
+
+var jd = {
+  name: 'JD',
+  age: 43,
+  hobbies: ['pickleball', 'fishing'],
+  haveBirthday: function () {
+    this.age++;
+  },
+  printSomething: function (txt) {
+    console.log(txt);
+  },
+  printAge: function () {
+    console.log(this.age);
+  },
+  printHobbiesByIndex: function (index) {
+    console.log(this.hobbies[index]);
+  }
+}
+
+// jd.printAge();
+
+var un = {
+  something: ['apple', 'orange']
+};
+
+
+// const fruits = ['apple', 'orange', 'grape', { name: 'JD' }, ['one', 'two']];
+
+// var result = fruits.includes(['one', 'two']);
+
+// var 
+
+
+
+
+
+
+
+
+
+
+
+// console.log(un.blah);
+
+// jd.printHobbiesByIndex(0);
+
+// joe.printSomething('some different text');
+// jd.printAge();
+
+// jd.haveBirthday();
+
+// jd.printAge();
+
+
+
+// function someFunc() {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 var oldStudentData = ['Matthew', 'FSF'];
@@ -16,7 +140,23 @@ var studentData = {
 };
 
 
-studentData.printInfo();
+// studentData.printInfo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var studentName = students[0];
 
